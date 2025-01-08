0527fcce6dfd2f78b1810ee7c6aed0184c655188
trucupie <wilgil3@gmail.com>
2025-01-08 17:28:05 -0500

    hoisting

---
+++ hoisting/index.js
@@ -0,0 +1,22 @@
+let destination = 'Ancient egypt';
+console.log(destination);
+
+destination = 'Medieval Europe';
+console.log(destination);
+
+const travelDate = '2024-03-15';
+// travelDate = '2024-03-16';
+/*
+ * Observations:
+ * Received error: TypeError: Assignment to constant variable.
+ */
+
+console.log(timeMachineModel);
+
+var timeMachineModel = 'T-800';
+
+/*
+ * Observations:
+ * defore timeMachineModel was declared received the following error: ReferenceError: timeMachineModel is not defined
+ * after timeMachineModel is showing as undefined
+ */
